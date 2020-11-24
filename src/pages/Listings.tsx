05625c0cafc3dd61b458fcc20dbd8d2da278034b
Aron Smith-Donovan <aronsmithdonovan@gmail.com>
2020-11-23 19:08:30 -0600

    added all sample listings

--- src/pages/Listings.tsx
+++ src/pages/Listings.tsx
@@ -20,55 +20,213 @@ const Listings: React.FC = () => {
           </IonButtons>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen color= "thisapp">
-          <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve">
-            <IonGrid>
-              <IonRow>
-                <IonCol><IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail></IonCol>
-                <IonCol size = "6">
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
                 <IonCardHeader>
-                 
-                <IonCardTitle>Naomi Family Residence</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>
+                  <IonCardTitle>Dorothy Day Center </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  Address: 183 Old Sixth Street Saint Paul, MN 55102 <br></br>
+                  Phone number: (651) 647-2350 <br></br>
+                  Website: https://www.cctwincities.org/locations/
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+
+              <IonCol size = "6">
+                <IonCardHeader>
+                  <IonCardTitle>Naomi Family Residence</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
                   Address: 77 E 9th Street St. Paul, MN 55101 <br></br>
                   Phone number: (651) 228-1800 <br></br>
-                  Website: https://www.ugmtc.org 
-                    
-                  </IonCardContent>
-                </IonCol>
-                <IonCol><IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail></IonCol>
-                <IonCol size = "6">
+                  Website: https://www.ugmtc.org
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
                 <IonCardHeader>
-                 
-                <IonCardTitle>The Family Place </IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>
+                  <IonCardTitle>The Family Place </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
                   Address: 244 Tenth Street East Saint Paul, MN 55101 <br></br>
                   Phone number: (651) 225-9354 <br></br>
                   Website: http://famplace.org/ 
-                    
-                  </IonCardContent>
-                </IonCol>
-                <IonCol><IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail></IonCol>
-                <IonCol size = "6">
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
                 <IonCardHeader>
-                 
-                <IonCardTitle>Union Gospel Mission Twin Cities </IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>
+                  <IonCardTitle>Union Gospel Mission Twin Cities </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
                   Address: 435 East University Avenue St. Paul, MN 55101 <br></br>
                   Phone number: (612) 292-1721 <br></br>
                   Website: https://www.ugmtc.org/ 
-                    
-                  </IonCardContent>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCard>
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
+                <IonCardHeader>
+                  <IonCardTitle>Higher Ground Saint Paul Shelter </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  Address: 435 Dorothy Day Place St. Paul, MN 55102 <br></br>
+                  Phone number: (651) 647-2330 <br></br>
+                  Website: https://www.cctwincities.org/locations/higher-ground-saint-paul/
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
+                <IonCardHeader>
+                  <IonCardTitle>Kingdom Pathways Freedom Outreach </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  Address: 1088 University Ave W St. Paul, MN 55104 <br></br>
+                  Phone number: (651) 387-8393 <br></br>
+                  Website: http://www.kingdompathways.org/
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
+                <IonCardHeader>
+                  <IonCardTitle>Catholic Charities Family Service Center </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  Address: 2001 Van Dyke Street Maplewood, MN 55109 <br></br>
+                  Phone number: (651) 291-0211 <br></br>
+                  Website: https://www.cctwincities.org/locations/family-service-center/
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
+                <IonCardHeader>
+                  <IonCardTitle>People Serving People Minneapolis </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  Address: People Serving People Minneapolis <br></br>
+                  Phone number: (612) 332-4500 <br></br>
+                  Website: http://www.peopleservingpeople.org
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
+                <IonCardHeader>
+                  <IonCardTitle>Hope Street Shelter for Young Adults </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  Address: 1121 East 46th Street Minneapolis, MN 55407 <br></br>
+                  Phone number: (612) 827-9372 <br></br>
+                  Website: https://www.cctwincities.org/locations/hope-street-shelter/
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+
+        <IonCard color = "dark" class="ion-card" className="ion-activated" href="./reserve-naomi-family-residence">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail>
+              </IonCol>
+              <IonCol size = "6">
+                <IonCardHeader>
+                  <IonCardTitle>Mary's Place </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  Address: 401 North 7th St Minneapolis, MN 55405 <br></br>
+                  Phone number: (612) 338-4855 <br></br>
+                  Website: https://sharingandcaringhands.org/
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
 };
+
 
 export default Listings;
