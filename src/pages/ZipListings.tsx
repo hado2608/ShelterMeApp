500d7daed1fba080526ac29c53911f8a7424fb33
Aron Smith-Donovan <aronsmithdonovan@gmail.com>
2020-11-23 20:20:23 -0600

    updated file path

--- src/pages/ZipListings.tsx
+++ src/pages/ZipListings.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonList, IonListHeader, IonMenuButton, IonPage, IonRow, IonTextarea, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
 import { listingData} from '../components/listingData';
-import './ZipListings.css';
+import './Listings.css';
 
 
 const ZipListings: React.FC = () => {
