0b39f9d64ee4269d0a73c9ccb6ecad18b41793f4
Aron Smith-Donovan <aronsmithdonovan@gmail.com>
2020-11-23 18:14:10 -0600

    added listings

--- src/pages/Listings.tsx
+++ src/pages/Listings.tsx
@@ -28,16 +28,41 @@ const Listings: React.FC = () => {
                 <IonCol size = "6">
                 <IonCardHeader>
                  
-                <IonCardTitle>Dorothy Day Center </IonCardTitle>
+                <IonCardTitle>Naomi Family Residence</IonCardTitle>
                   </IonCardHeader>
                   <IonCardContent>
-                  Address: 183 Old Sixth Street Saint Paul, MN 55102 <br></br>
-                  Phone number: (651) 647-2350 <br></br>
-                  Website: https://www.cctwincities.org/locations/ 
+                  Address: 77 E 9th Street St. Paul, MN 55101 <br></br>
+                  Phone number: (651) 228-1800 <br></br>
+                  Website: https://www.ugmtc.org 
                     
                   </IonCardContent>
                 </IonCol>
-                <IonCol></IonCol>
+                <IonCol><IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail></IonCol>
+                <IonCol size = "6">
+                <IonCardHeader>
+                 
+                <IonCardTitle>The Family Place </IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                  Address: 244 Tenth Street East Saint Paul, MN 55101 <br></br>
+                  Phone number: (651) 225-9354 <br></br>
+                  Website: http://famplace.org/ 
+                    
+                  </IonCardContent>
+                </IonCol>
+                <IonCol><IonThumbnail class = "listing_img"><IonImg src = {"./assets/img/listingdemo.png"}></IonImg></IonThumbnail></IonCol>
+                <IonCol size = "6">
+                <IonCardHeader>
+                 
+                <IonCardTitle>Union Gospel Mission Twin Cities </IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                  Address: 435 East University Avenue St. Paul, MN 55101 <br></br>
+                  Phone number: (612) 292-1721 <br></br>
+                  Website: https://www.ugmtc.org/ 
+                    
+                  </IonCardContent>
+                </IonCol>
               </IonRow>
             </IonGrid>
           </IonCard>
