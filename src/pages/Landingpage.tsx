88f993013ecacbb88093be7050f19fc3220b01c7
Aron Smith-Donovan <aronsmithdonovan@gmail.com>
2020-11-23 20:29:46 -0600

    added filter buttons

--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonButton, IonButtons, IonContent, IonHeader, IonImg, IonMenuButton, IonPage, IonSearchbar, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonImg, IonMenuButton, IonPage, IonRow, IonSearchbar, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Landingpage.css';
 
@@ -24,6 +24,100 @@ const Landingpage: React.FC = () => {
         <IonSearchbar color="white" class = 'center' value={searchText} onIonChange={e => setSearchText(e.detail.value!)} 
         inputmode="numeric" type="number"></IonSearchbar>
         <IonButton id = "search_button" color = "#79acb9" href = "/search-results">Search</IonButton>
+      {/* </IonContent>
+      <IonContent> */}
+        <IonCard color = 'header'>
+          <IonHeader class = 'amenities' color = 'header'><br></br>Or, select an amenity:<br></br><br></br></IonHeader>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonButton class = 'buttons' color="light" href = "/filter?=women-only">Women only</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light" href = "/filter?=meals-provided">Meals provided</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Shower facilities</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Laundry facilities</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Medical care available</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Mail facilities</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Veteran services</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Religious affiliation</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Addiction and rehabilitation support</IonButton>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Mental health services</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Education resources</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Day shelter only</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Overnight shelter only</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Women and children only</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Transitional housing</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Social services</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Childcare available</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Men only</IonButton>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Pay-to-stay shelter</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Long-term care</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Felons allowed</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Job training available</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Family shelter</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Computer lab</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Youth shelter</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Clothing provided</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton class = 'buttons' color="light">Bus cards available</IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
